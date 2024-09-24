8c18ac06e7340ca9b029a095ae6bed98da1a4c8a
KotSnozom <166803529+TheCodCat@users.noreply.github.com>
2024-09-24 21:33:04 +0400

    add afect

--- Assets/Scripts/CutScene/StartCutScene/CutScene.cs
+++ Assets/Scripts/CutScene/StartCutScene/CutScene.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class CutScene : MonoBehaviour
 {
     [SerializeField] private string _nameScene;
+    [SerializeField] private PlayableDirector _director;
+    [SerializeField] private GameObject[] _gameObjects;
 
+    public void StartButton()
+    {
+        _director.Play();
+        foreach (var item in _gameObjects)
+        {
+            item.SetActive(false);
+        }
+    }
 
     public void LoadScene()
     {
         StartCoroutine(Loader());
-
-        IEnumerator Loader() 
-        {
-            SceneManager.LoadSceneAsync(_nameScene);
-            yield return null;
-        }
+    }
+    IEnumerator Loader()
+    {
+        SceneManager.LoadSceneAsync(_nameScene);
+        yield return null;
     }
 }
