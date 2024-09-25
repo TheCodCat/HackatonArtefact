50d1b19e07ff3ee86ffcdea3d98a43cdda44d467
KotSnozom <166803529+TheCodCat@users.noreply.github.com>
2024-09-25 23:51:15 +0400

    Beta StartCutscene

--- Assets/Scripts/CutScene/StartCutScene/CutScene.cs
+++ Assets/Scripts/CutScene/StartCutScene/CutScene.cs
@@ -1,24 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class CutScene : MonoBehaviour
 {
     [SerializeField] private string _nameScene;
-    [SerializeField] private PlayableDirector _director;
+    [SerializeField] private Playable[] _directors;
+    [SerializeField] private PlayableDirector _currentDirector;
     [SerializeField] private GameObject[] _gameObjects;
+    private int _indexCutscene = 0;
 
-    public void StartButton()
+    private void Start()
     {
-        _director.Play();
-        foreach (var item in _gameObjects)
+        StartCoroutine(CutScensManager());
+    }
+    IEnumerator CutScensManager()
+    {
+        while(_indexCutscene < _directors.Length - 1)
         {
-            item.SetActive(false);
+            Debug.Log(_currentDirector.duration);
+            yield return new WaitForSeconds((float)_currentDirector.duration);
+            Skip();
+        }
+
+    }
+    public void Skip()
+    {
+        if (_indexCutscene < _directors.Length - 1)
+        {
+            for (int i = 0; i < _directors.Length - 1; i++)
+            {
+                _directors[_indexCutscene].Objects[i].gameObject.SetActive(false);
+            }
+            _currentDirector.Stop();
+            _indexCutscene++;
+            _currentDirector = _directors[_indexCutscene].PlayableDirector;
+            _currentDirector.Play();
         }
     }
-
+    public void Skip(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (_indexCutscene < _directors.Length - 1)
+            {
+                for (int i = 0; i < _directors.Length - 1; i++)
+                {
+                    _directors[_indexCutscene].Objects[i].gameObject.SetActive(false);
+                }
+                _currentDirector.Stop();
+                _indexCutscene++;
+                _currentDirector = _directors[_indexCutscene].PlayableDirector;
+                _currentDirector.Play();
+            }
+            else
+            {
+                if(_currentDirector != null) _currentDirector.Stop();
+                LoadScene();
+            }
+        }
+    }
     public void LoadScene()
     {
         StartCoroutine(Loader());
@@ -28,4 +72,10 @@ public class CutScene : MonoBehaviour
         SceneManager.LoadSceneAsync(_nameScene);
         yield return null;
     }
+}
+[System.Serializable]
+public class Playable
+{
+    public PlayableDirector PlayableDirector;
+    public List<GameObject> Objects;
 }
