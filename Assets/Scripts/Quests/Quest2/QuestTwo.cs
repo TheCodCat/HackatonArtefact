3b246fafa0de74a88044a6825746b9a630679a99
TheCodCat <166803529+TheCodCat@users.noreply.github.com>
2024-09-29 01:19:42 +0400

    Add

--- Assets/Scripts/Quests/Quest2/QuestTwo.cs
+++ Assets/Scripts/Quests/Quest2/QuestTwo.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestTwo : MonoBehaviour
 {
     [SerializeField] private int _units, _tens, _hundreds;
-    public void GetNumbers(Discharge discharge)
+    [SerializeField] private Text _unitsT,_tentT,_hundredsT;
+	[SerializeField] private GameObject _kupol;
+    [SerializeField] private int _current;
+    [SerializeField] private int _necessary;
+
+	public void GetNumbers(Discharge discharge)
     {
         switch (discharge)
         {
             case Discharge.Units:
                 _units = (_units + 1) % 10;
-                break;
+                _unitsT.text = _units.ToString();
+				break;
             case Discharge.Tens:
                 _tens = (_tens + 10) % 100;
-                break;
+				_tentT.text = _tens.ToString();
+				break;
             case Discharge.Hundreds:
                 _hundreds = (_hundreds + 100) % 1000;
-                break;
+				_hundredsT.text = _hundreds.ToString();
+				break;
         }
-        Debug.Log(_units + _tens + _hundreds);
+
+		_current = _units + _tens + _hundreds;
+        if(_current == _necessary) _kupol.SetActive(false);
     }
 }
